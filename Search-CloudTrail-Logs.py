2dd214289b635d7f460f01d31991a3ac8daa64e8
Aaron Dhiman <aaron.dhiman@gmail.com>
2019-12-23 10:13:32 -0500

    implement argument for search string.  Only print messages matching the search string

--- Search-CloudTrail-Logs.py
+++ Search-CloudTrail-Logs.py
@@ -1,9 +1,10 @@
 import boto3
 import sys
-#import json
-#import time
+import re
 import pprint
 #from datetime import date
+#import json
+#import time
 
 client = boto3.client('cloudtrail')
 responseDict = client.lookup_events(
@@ -22,12 +23,18 @@ responseDict = client.lookup_events(
 #for item in responseDict:
 #	print(item)
 
+searchPattern = sys.argv[1]
+
 eventsList = responseDict['Events']
 #print(events[0]['Username'])
 pp = pprint.PrettyPrinter(indent=4)
 i=0
 while i < len(eventsList):
-    pp.pprint(eventsList[i])
+    p = re.compile('.*(%s).*'%searchPattern)
+    m = p.search(eventsList[i]['CloudTrailEvent'])
+    if m:
+        #print(m)
+        pp.pprint(eventsList[i]['CloudTrailEvent'])
+        print('----------------------------------------------------')
     i=i+1
-    print('----------------------------------------------------')
 
