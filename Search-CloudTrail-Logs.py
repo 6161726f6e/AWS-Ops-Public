7641b3a4b7e715621029a8c204b233c53cb2b481
usera <usera@pop-os.localdomain>
2021-01-27 12:22:14 -0500

    convert from global variables to local scopes

--- Search-CloudTrail-Logs.py
+++ Search-CloudTrail-Logs.py
@@ -15,11 +15,6 @@ import pprint
 import argparse
 from datetime import datetime, date, time
 
-# global variables
-searchPattern = ''		# default: blank filter
-startDate = datetime.combine(date.today(), time())
-endDate = datetime.now()
-
 def getArgs():
     global searchPattern, startDate, endDate
     
@@ -28,8 +23,8 @@ def getArgs():
     parser = argparse.ArgumentParser(description = helpText)
     parser.add_argument("-V", "--version", help="Show program version", action="store_true")
     parser.add_argument("-p", "--pattern", action='store', type=str, help="Pattern to search for.  Default is \'\'")
-    parser.add_argument("-s", "--startdate",  action='store', type=str, help="UTC Time to start search for. Format = 2019-12-25-hh:mm")
-    parser.add_argument("-e", "--enddate",  action='store', type=str, help="UTC Time to end search for. Format = 2019-12-25-hh:mm")
+    parser.add_argument("-s", "--startDate",  action='store', type=str, help="UTC Time to start search for. Format = 2019-12-25-hh:mm")
+    parser.add_argument("-e", "--endDate",  action='store', type=str, help="UTC Time to end search for. Format = 2019-12-25-hh:mm")
 
     # read arguments from the command line
     args = parser.parse_args()
@@ -43,16 +38,24 @@ def getArgs():
     if args.pattern:
     	print("setting pattern "+str(args.pattern))
     	searchPattern = args.pattern
+    else:
+        searchPattern = ''      # default: blank filter
 
-    if args.startdate:
-    	print("setting startdate "+str(args.startdate))
-    	startDate = args.startdate
+    if args.startDate:
+    	print("setting startDate "+str(args.startDate))
+    	startDate = args.startDate
+    else:
+        startDate = datetime.combine(date.today(), time())
 
-    if args.enddate:
-    	print("setting enddate "+str(args.enddate))
-    	endDate = args.enddate
+    if args.endDate:
+    	print("setting endDate "+str(args.endDate))
+    	endDate = args.endDate
+    else:
+        endDate = datetime.now()
 
-def pullLogs(searchPattern):
+    return(searchPattern,startDate,endDate)
+
+def pullLogs(searchPattern,startDate,endDate):
     client = boto3.client('cloudtrail')
     responseDict = client.lookup_events(
         StartTime=startDate,
@@ -84,5 +87,5 @@ def pullLogs(searchPattern):
     print('*****************************************************')
 
 if __name__ == "__main__":
-    getArgs()
-    pullLogs(searchPattern)
+    searchPattern,startDate,endDate=getArgs()
+    pullLogs(searchPattern,startDate,endDate)
